b120e347addb4f73f6be984acf3b1ae794703226
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-01-15 02:00:43 +0530

    Added prisma mutation code

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -14,3 +14,22 @@ prisma.query.users(null,'{ id name review { name } }')
 prisma.query.comments(null, '{ id title body author{ id name} }')
     .then(data=> console.log(JSON.stringify(data, undefined,2)))
     .catch(err => console.log(err.message))
+
+const createCommentString = `data:{
+    id:5,
+    title:"The noodles are straight from heaven",
+    body:"The sauce and vegetables are cooked to perfection, taking a meal there to paradise",
+    author:{
+      connect:{
+        id:4
+      }
+    }
+    review:{
+      connect:{
+        id:2
+      }
+    }
+}`
+prisma.mutation.createComment(createCommentString,'id title body')
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message); )
